86301c47061902452bd5fc8486d1833afe186e64
david <davidsanchezdiaz72@gmail.com>
2023-12-03 22:39:02 +0100

    prueba

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+try {
+    $conexion = new PDO('mysql:host=localhost;dbname=paginacion', 'root', '');
+}catch (PDOException $e) {
+    echo "ERROR:" . $e->getMessage();
+    die();
+}
+
+$pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+$post_pagina = 5;
+
+$inicio = ($pagina > 1) ? ($pagina * $post_pagina - $post_pagina) : 0;
+
+$articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $post_pagina");
+
+$articulos->execute();
+$articulos = $articulos->fetchAll();
+
+if(!$articulos) {
+    header('Location: index.php');
+}
+
+$total_articulos = $conexion->query('SELECT FOUND_ROWS() as total');
+$total_articulos = $total_articulos->fetchAll()[0]['total'];
+$numero_paginas = ceil($total_articulos / $post_pagina);
+
+require 'index_view.php';
---
+++ index_view.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Paginación</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Articulos</h1>
+        <section class="articulos">
+
+            <ul>
+                <?php foreach ($articulos as $articulo): ?>
+                    <li><?php echo $articulo['id'] . '.- ' . $articulo['articulo'] ?></li>
+                <?php endforeach; ?>   
+            </ul>
+        </section>
+
+        <section class="paginacion">
+            <ul>
+                <!-- Establecemos cuando el boton de "Anterior" estará desabilitado -->
+                <?php if ($pagina == 1): ?>
+                    <li class="disabled">&laquo;</li>
+                <?php else: ?>
+                    <li><a href="?pagina=<?php echo $pagina - 1 ?>">&laquo;</a></li>
+                <?php endif; ?>
+
+                <!-- Ejecutamos un ciclo para mostrar lñas paginas -->
+                <?php
+                    for($i = 1; $i <= $numero_paginas; $i++){
+                        if ($pagina == $i) {
+                            echo "<li class='active'><a href='?pagina = $i'>$i</a></li>";
+                        } else {
+                            echo "<li><a href='?pagina = $i'>$i</a></li>";
+                        }
+                    }
+                ?>
+
+                <!-- Establecemos cuando el boton de "Siguente" estará desabilitado -->
+                <?php if ($pagina == $numero_paginas): ?>
+                    <li class="disabled">&raquo;</li>
+                <?php else: ?>
+                    <li><a href="?pagina=<?php echo $pagina + 1 ?>">&raquo;</a></li>
+                <?php endif; ?>
+            </ul>    
+        </section>
+    </div>
+</body>
+</html>
